f0813a149d13ea9cb57be03d71c700ec6f609ddd
Samuil Hopkins <x2015xx@gmail.com>
2026-04-01 23:39:01 +0300

    feat: add custom exception

---
+++ src/Exception/WhatsAppMediaException.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WhatsApp\Media\Exception;
+
+final class WhatsAppMediaException extends \RuntimeException
+{
+    /**
+     * @param string $message
+     * @return self
+     */
+    public static function encryptionFailed(string $message): self
+    {
+        return new self('Encryption failed: ' . $message);
+    }
+
+    /**
+     * @param string $message
+     * @return self
+     */
+    public static function decryptionFailed(string $message): self
+    {
+        return new self('Decryption failed: ' . $message);
+    }
+
+    /**
+     * @param string $message
+     * @return self
+     */
+    public static function invalidSidecar(string $message): self
+    {
+        return new self('Invalid sidecar: ' . $message);
+    }
+}
