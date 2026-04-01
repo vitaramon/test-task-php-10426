a52b6048eee2e919d3ffd3766739f4f0ddd454e8
Samuil Hopkins <x2015xx@gmail.com>
2026-04-02 02:23:16 +0300

    feat: add DecryptingStream

---
+++ src/Stream/DecryptingStream.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WhatsApp\Media;
+
+use GuzzleHttp\Psr7\StreamDecoratorTrait;
+use Psr\Http\Message\StreamInterface;
+use WhatsApp\Media\Crypto\MediaCrypter;
+use WhatsApp\Media\Enum\MediaType;
+use WhatsApp\Media\Exception\WhatsAppMediaException;
+
+final class DecryptingStream implements StreamInterface
+{
+    use StreamDecoratorTrait;
+
+    private StreamInterface $stream;
+    private string $mediaKey;
+    private MediaType $type;
+    private ?string $decryptedBuffer = null;
+
+    public function __construct(StreamInterface $encryptedStream, string $mediaKey, MediaType $type)
+    {
+        $this->stream = $encryptedStream;
+        $this->mediaKey = $mediaKey;
+        $this->type = $type;
+    }
+
+    /**
+     * @param $length
+     * @return string
+     */
+    public function read($length): string
+    {
+        if ($this->decryptedBuffer === null) {
+            $data = $this->stream->getContents();
+            $this->decryptedBuffer = MediaCrypter::decrypt($data, $this->mediaKey, $this->type);
+        }
+
+        $out = substr($this->decryptedBuffer, 0, $length);
+        $this->decryptedBuffer = substr($this->decryptedBuffer, $length);
+        return $out;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getSize(): ?int
+    {
+        return $this->decryptedBuffer !== null ? strlen($this->decryptedBuffer) : null;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isSeekable(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @param $offset
+     * @param $whence
+     * @return void
+     */
+    public function seek($offset, $whence = SEEK_SET): void
+    {
+        throw WhatsAppMediaException::decryptionFailed('Stream is not seekable (MAC validation requires full read)');
+    }
+
+    /**
+     * @return void
+     */
+    public function rewind(): void
+    {
+        throw WhatsAppMediaException::decryptionFailed('Stream is not seekable');
+    }
+
+    /**
+     * @return bool
+     */
+    public function isWritable(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @param $string
+     * @return int
+     */
+    public function write($string): int
+    {
+        throw WhatsAppMediaException::decryptionFailed('Stream is read-only');
+    }
+}
